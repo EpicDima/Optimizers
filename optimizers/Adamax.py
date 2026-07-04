a347fc126acdebd208912a4b251eb962176c8fbf
EpicDima <dima2-8@bk.ru>
2026-07-04 04:08:11 +0300

    Adamax: bias-коррекция шага и eps внутри max-аккумулятора, как в PyTorch

--- optimizers/Adamax.py
+++ optimizers/Adamax.py
@@ -4,6 +4,7 @@ from .Optimizer import Optimizer, np
 class Adamax(Optimizer):
     v: np.ndarray
     acc: np.ndarray
+    t: int
 
     def __init__(
         self,
@@ -18,14 +19,16 @@ class Adamax(Optimizer):
         super().__init__(initial_x, function, params)
 
     def next_point(self) -> tuple[np.ndarray, float]:
+        self.t += 1
         gradient = self.function.grad(self.x)
         self.v = self.params["beta1"] * self.v + (1 - self.params["beta1"]) * gradient
-        self.acc = np.maximum(self.params["beta2"] * self.acc, np.fabs(gradient))
-        adaptive_lr = self.params["lr"] / (self.acc + self.params["eps"])
-        next_x = self.x - adaptive_lr * self.v
+        self.acc = np.maximum(self.params["beta2"] * self.acc, np.fabs(gradient) + self.params["eps"])
+        clr = self.params["lr"] / (1 - self.params["beta1"] ** self.t)
+        next_x = self.x - clr * self.v / self.acc
         return self.move_next(next_x)
 
     def reset(self) -> None:
         super().reset()
         self.v = np.zeros([2])
         self.acc = np.zeros([2])
+        self.t = 0
