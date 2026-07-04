e67faa989a9794960136cc1a7a84bb91b61e58ba
EpicDima <dima2-8@bk.ru>
2026-07-04 17:51:36 +0300

    Добавлены классические расписания скорости обучения

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -1,1 +1,6 @@
 from .Constant import Constant
+from .ExponentialDecay import ExponentialDecay
+from .InverseTimeDecay import InverseTimeDecay
+from .LinearDecay import LinearDecay
+from .PolynomialDecay import PolynomialDecay
+from .StepDecay import StepDecay
