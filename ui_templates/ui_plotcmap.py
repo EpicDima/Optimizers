fe3c80c1c2f90783115d3dc8397f0bb67d2559b6
EpicDima <dima2-8@bk.ru>
2026-07-04 03:31:55 +0300

    Перегенерация UI-шаблонов через pyside6-uic (Qt 6.11)

--- ui_templates/ui_plotcmap.py
+++ ui_templates/ui_plotcmap.py
@@ -1,46 +1,55 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'plot_cmap_ui.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'plot_cmap_ui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.11.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QDialog, QListWidget,
+    QListWidgetItem, QPushButton, QSizePolicy, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
         Dialog.resize(270, 300)
-        self.reverse_checkbox = QtWidgets.QCheckBox(Dialog)
-        self.reverse_checkbox.setGeometry(QtCore.QRect(10, 230, 150, 20))
-        font = QtGui.QFont()
+        self.reverse_checkbox = QCheckBox(Dialog)
+        self.reverse_checkbox.setObjectName(u"reverse_checkbox")
+        self.reverse_checkbox.setGeometry(QRect(10, 230, 150, 20))
+        font = QFont()
         font.setPointSize(9)
         self.reverse_checkbox.setFont(font)
-        self.reverse_checkbox.setObjectName("reverse_checkbox")
-        self.ok_button = QtWidgets.QPushButton(Dialog)
-        self.ok_button.setGeometry(QtCore.QRect(190, 260, 70, 25))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.ok_button = QPushButton(Dialog)
+        self.ok_button.setObjectName(u"ok_button")
+        self.ok_button.setGeometry(QRect(190, 260, 70, 25))
         self.ok_button.setFont(font)
-        self.ok_button.setObjectName("ok_button")
-        self.cancel_button = QtWidgets.QPushButton(Dialog)
-        self.cancel_button.setGeometry(QtCore.QRect(100, 260, 70, 25))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.cancel_button = QPushButton(Dialog)
+        self.cancel_button.setObjectName(u"cancel_button")
+        self.cancel_button.setGeometry(QRect(100, 260, 70, 25))
         self.cancel_button.setFont(font)
-        self.cancel_button.setObjectName("cancel_button")
-        self.cmap_list = QtWidgets.QListWidget(Dialog)
-        self.cmap_list.setGeometry(QtCore.QRect(10, 10, 150, 210))
-        self.cmap_list.setObjectName("cmap_list")
+        self.cmap_list = QListWidget(Dialog)
+        self.cmap_list.setObjectName(u"cmap_list")
+        self.cmap_list.setGeometry(QRect(10, 10, 150, 210))
 
         self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
 
     def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Цветовая схема"))
-        self.reverse_checkbox.setText(_translate("Dialog", "Обратный (Reversed)"))
-        self.ok_button.setText(_translate("Dialog", "OK"))
-        self.cancel_button.setText(_translate("Dialog", "Отмена"))
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u0426\u0432\u0435\u0442\u043e\u0432\u0430\u044f \u0441\u0445\u0435\u043c\u0430", None))
+        self.reverse_checkbox.setText(QCoreApplication.translate("Dialog", u"\u041e\u0431\u0440\u0430\u0442\u043d\u044b\u0439 (Reversed)", None))
+        self.ok_button.setText(QCoreApplication.translate("Dialog", u"OK", None))
+        self.cancel_button.setText(QCoreApplication.translate("Dialog", u"\u041e\u0442\u043c\u0435\u043d\u0430", None))
+    # retranslateUi
 
--- ui_templates/ui_plottype.py
+++ ui_templates/ui_plottype.py
@@ -1,66 +1,69 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'plot_type_ui.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'plot_type_ui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.11.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QLineEdit,
+    QPushButton, QRadioButton, QSizePolicy, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
         Dialog.resize(400, 150)
-        self.number_edit = QtWidgets.QLineEdit(Dialog)
-        self.number_edit.setGeometry(QtCore.QRect(320, 30, 70, 20))
-        font = QtGui.QFont()
+        self.number_edit = QLineEdit(Dialog)
+        self.number_edit.setObjectName(u"number_edit")
+        self.number_edit.setGeometry(QRect(320, 30, 70, 20))
+        font = QFont()
         font.setPointSize(9)
         self.number_edit.setFont(font)
-        self.number_edit.setObjectName("number_edit")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(210, 30, 101, 20))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(210, 30, 101, 20))
         self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.cancel_button = QtWidgets.QPushButton(Dialog)
-        self.cancel_button.setGeometry(QtCore.QRect(230, 110, 70, 25))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.cancel_button = QPushButton(Dialog)
+        self.cancel_button.setObjectName(u"cancel_button")
+        self.cancel_button.setGeometry(QRect(230, 110, 70, 25))
         self.cancel_button.setFont(font)
-        self.cancel_button.setObjectName("cancel_button")
-        self.ok_button = QtWidgets.QPushButton(Dialog)
-        self.ok_button.setGeometry(QtCore.QRect(320, 110, 70, 25))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.ok_button = QPushButton(Dialog)
+        self.ok_button.setObjectName(u"ok_button")
+        self.ok_button.setGeometry(QRect(320, 110, 70, 25))
         self.ok_button.setFont(font)
-        self.ok_button.setObjectName("ok_button")
-        self.widget = QtWidgets.QWidget(Dialog)
-        self.widget.setGeometry(QtCore.QRect(20, 20, 160, 80))
-        self.widget.setObjectName("widget")
-        self.contour_radio = QtWidgets.QRadioButton(self.widget)
-        self.contour_radio.setGeometry(QtCore.QRect(10, 10, 140, 20))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.widget = QWidget(Dialog)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(20, 20, 160, 80))
+        self.contour_radio = QRadioButton(self.widget)
+        self.contour_radio.setObjectName(u"contour_radio")
+        self.contour_radio.setGeometry(QRect(10, 10, 140, 20))
         self.contour_radio.setFont(font)
-        self.contour_radio.setObjectName("contour_radio")
-        self.mesh_radio = QtWidgets.QRadioButton(self.widget)
-        self.mesh_radio.setGeometry(QtCore.QRect(10, 50, 140, 20))
-        font = QtGui.QFont()
-        font.setPointSize(9)
+        self.mesh_radio = QRadioButton(self.widget)
+        self.mesh_radio.setObjectName(u"mesh_radio")
+        self.mesh_radio.setGeometry(QRect(10, 50, 140, 20))
         self.mesh_radio.setFont(font)
-        self.mesh_radio.setObjectName("mesh_radio")
 
         self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
 
     def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Тип графика"))
-        self.label.setText(_translate("Dialog", "Число контуров"))
-        self.cancel_button.setText(_translate("Dialog", "Отмена"))
-        self.ok_button.setText(_translate("Dialog", "OK"))
-        self.contour_radio.setText(_translate("Dialog", "Контурный график"))
-        self.mesh_radio.setText(_translate("Dialog", "Сетчатый график"))
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u0422\u0438\u043f \u0433\u0440\u0430\u0444\u0438\u043a\u0430", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"\u0427\u0438\u0441\u043b\u043e \u043a\u043e\u043d\u0442\u0443\u0440\u043e\u0432", None))
+        self.cancel_button.setText(QCoreApplication.translate("Dialog", u"\u041e\u0442\u043c\u0435\u043d\u0430", None))
+        self.ok_button.setText(QCoreApplication.translate("Dialog", u"OK", None))
+        self.contour_radio.setText(QCoreApplication.translate("Dialog", u"\u041a\u043e\u043d\u0442\u0443\u0440\u043d\u044b\u0439 \u0433\u0440\u0430\u0444\u0438\u043a", None))
+        self.mesh_radio.setText(QCoreApplication.translate("Dialog", u"\u0421\u0435\u0442\u0447\u0430\u0442\u044b\u0439 \u0433\u0440\u0430\u0444\u0438\u043a", None))
+    # retranslateUi
 
