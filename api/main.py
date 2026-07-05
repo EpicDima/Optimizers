a5b9b7791d8c1d40a5483144741c898b8ac7c4a4
EpicDima <dima2-8@bk.ru>
2026-07-05 19:17:55 +0300

    Порты бэкенда и dev-сервера сделаны менее стандартными
    localhost:8000/5173 слишком часто заняты другими локальными сервисами;
    заменены на менее популярные 58217/47913.

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Optimizers API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:47913", "http://127.0.0.1:47913"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
