fa3cb3e071511a92d1a236424580d7fb2c875d67
EpicDima <dima2-8@bk.ru>
2026-07-08 02:49:13 +0300

    Настройки графика (палитра, вид, уровни) сохраняются в localStorage

--- src/entities/plot-settings/store.ts
+++ src/entities/plot-settings/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import { DEFAULT_TAIL } from "@shared/config/constants";
 
@@ -14,19 +15,24 @@ interface PlotSettingsState extends PlotSettings {
   setShowGradientField: (show: boolean) => void;
 }
 
-export const usePlotSettingsStore = create<PlotSettingsState>((set) => ({
-  is3D: false,
-  contourMode: "contour",
-  contourLevels: 30,
-  colormap: "inferno",
-  colormapReversed: false,
-  tailLength: DEFAULT_TAIL,
-  showGradientField: false,
-  setIs3D: (is3D) => set({ is3D }),
-  setContourMode: (contourMode) => set({ contourMode }),
-  setContourLevels: (contourLevels) => set({ contourLevels }),
-  setColormap: (colormap) => set({ colormap }),
-  setColormapReversed: (colormapReversed) => set({ colormapReversed }),
-  setTailLength: (tailLength) => set({ tailLength }),
-  setShowGradientField: (showGradientField) => set({ showGradientField }),
-}));
+export const usePlotSettingsStore = create<PlotSettingsState>()(
+  persist(
+    (set) => ({
+      is3D: false,
+      contourMode: "contour",
+      contourLevels: 30,
+      colormap: "inferno",
+      colormapReversed: false,
+      tailLength: DEFAULT_TAIL,
+      showGradientField: false,
+      setIs3D: (is3D) => set({ is3D }),
+      setContourMode: (contourMode) => set({ contourMode }),
+      setContourLevels: (contourLevels) => set({ contourLevels }),
+      setColormap: (colormap) => set({ colormap }),
+      setColormapReversed: (colormapReversed) => set({ colormapReversed }),
+      setTailLength: (tailLength) => set({ tailLength }),
+      setShowGradientField: (showGradientField) => set({ showGradientField }),
+    }),
+    { name: "optimizers-plot-settings" },
+  ),
+);
